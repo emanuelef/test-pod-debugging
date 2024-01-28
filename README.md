7c3008f4171753abd417c56e80e8dd195ba264fd
Emanuele Fumagalli <emanuelef@gmail.com>
2024-01-28 03:30:21 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+`docker build -t simple-app .`
+
+`docker run -p 8082:8080 simple-app`
+
---
+++ app.py
@@ -0,0 +1,42 @@
+import asyncio
+from aiohttp import web
+
+
+async def handleRoot(request):
+    text = "Hello!"
+    return web.Response(text=text)
+
+
+async def handle(request):
+    name = request.match_info.get("name", "Anonymous")
+    text = f"Hello, {name}!"
+    return web.Response(text=text)
+
+
+async def init():
+    app = web.Application()
+    app.router.add_get("/", handleRoot)
+    app.router.add_get("/{name}", handle)
+
+    # Setup the aiohttp server
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    # Create the aiohttp server and start it
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
+    await site.start()
+
+    print("Server started on http://0.0.0.0:8080")
+
+
+# Run the asyncio event loop
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init())
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
