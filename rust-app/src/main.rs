41d16f4cb2041c7a6e2c745bbb203ee3b01dcebb
Emanuele Fumagalli <emanuelef@gmail.com>
2024-02-02 13:42:42 +0000

    retrigger rust build

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -17,10 +17,7 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Initialize the logger (env_logger in this case, but you can choose other loggers)
     env_logger::init();
-
-    // Log that the web server is starting
     log::info!("Starting web server on 0.0.0.0:8080");
 
     // Start the Actix web server
