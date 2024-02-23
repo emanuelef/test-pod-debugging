f312eb34ef7e9aa3f346877ae156349c10936b23
Emanuele Fumagalli <emanuelef@gmail.com>
2024-02-23 10:00:52 +0000

    Add logs

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
@@ -23,11 +23,12 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     log::warn!("Starting web server on 0.0.0.0:8080");
 
     // Start the Actix web server
-    HttpServer::new(|| App::new().service(greet))
+    HttpServer::new(|| App::new().wrap(Logger::default()).service(greet))
         .bind(("0.0.0.0", 8080))?
         .run()
         .await
